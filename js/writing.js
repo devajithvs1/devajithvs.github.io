bec62a0b476ace70e3a03f0f2b4e55330e7c724a
devajithvs <devajithvs@gmail.com>
2017-06-17 11:13:16 +0530

    Update writing.js

--- js/writing.js
+++ js/writing.js
@@ -2,9 +2,7 @@ window.onload=function() {
 (function() {
   var openComment, styles, time, writeStyleChar, writeStyles;
 
-  styles = "/* \n * \"Website v2\"\n * Author - Devajith\n */\n\nbody {\n  background-color: #1a1c24; color: #fff;\n  font-size: 11px; line-height: 1.4;\n  -webkit-font-smoothing: subpixel-antialiased;\n}\n\n/* ...\n *\n * Oh, hey! Didn't see you there!\n *\n * I was upgrading this website.\n * Sure, you can watch.\n *\n *\n * But \n *\n * DONT CLICK THAT BUTTON ON TOP\n *\n *\n *\n * Well..... you can wait here\n * \n * I will go and have a coffee...*/";
-
-
+  styles = "/* \n * \"Website v2\"\n * Author - Devajith\n */\n\nbody {\n  background-color: #1a1c24; color: #fff;\n  font-size: 11px; line-height: 1.4;\n  -webkit-font-smoothing: subpixel-antialiased;\n}\n\n/* ...\n *\n * Oh, hey! Didn't see you there!\n *\n * I was upgrading this website.\n * Sure, you can watch.\n *\n *\n * But \n *\n * DONT CLICK THAT BUTTON ON TOP\n*/\n \n pre { \n  position: fixed; width: 60%;\n  top: 30px; bottom: 30px; left: 30%;\n  transition: left 500ms;\n  overflow: auto;\n  background-color: #313744; color: #a6c3d4;\n  border: 1px solid rgba(0,0,0,0.2);\n  padding: 24px 12px;\n  box-sizing: border-box;\n  border-radius: 3px;\n  box-shadow: 0px 4px 0px 2px\n  rgba(0,0,0,0.1);\n}\n\n\n/* \n * Syntax highlighting \n */\n\npre em:not(.comment) { font-style: normal; }\n\n.comment       { color: #707e84; }\n.selector      { color: #c66c75; }\n.selector .key { color: #c66c75; }\n.key           { color: #c7ccd4; }\n.value         { color: #d5927b; }\n\n\n/* \n *  Hang on, let me just move this a bit*/\n\n pre { left: 40%; }\n\n\n/*\n * Much better!\n * Aah I'm too tired... \n * You can wait here \n * I will go and have a coffee...*/";
 
   openComment = false;
 
